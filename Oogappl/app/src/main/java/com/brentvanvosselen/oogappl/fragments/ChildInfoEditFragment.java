99dc838684ea984a05d1860f05c34bd2fff15e14
Joshi <joshi@MacBook-Pro-van-Joshi.local>
2017-10-30 16:35:24 +0100

    #1 childinfoedit remove items

--- Oogappl/app/src/main/java/com/brentvanvosselen/oogappl/fragments/ChildInfoEditFragment.java
+++ Oogappl/app/src/main/java/com/brentvanvosselen/oogappl/fragments/ChildInfoEditFragment.java
@@ -2,11 +2,17 @@ package com.brentvanvosselen.oogappl.fragments;
 
 import android.os.Bundle;
 import android.support.annotation.Nullable;
+import android.support.constraint.ConstraintLayout;
 import android.support.v4.app.Fragment;
+import android.util.Log;
 import android.view.LayoutInflater;
 import android.view.View;
 import android.view.ViewGroup;
+import android.widget.ImageButton;
+import android.widget.LinearLayout;
+import android.widget.TextView;
 
+import com.brentvanvosselen.oogappl.RestClient.Info;
 import com.brentvanvosselen.oogappl.util.ObjectSerializer;
 import com.brentvanvosselen.oogappl.R;
 import com.brentvanvosselen.oogappl.RestClient.Category;
@@ -21,14 +27,20 @@ public class ChildInfoEditFragment extends Fragment {
 
         Bundle bundle = getArguments();
         if(bundle != null) {
+            Log.i("ChildEdit", "Got category");
             category = ObjectSerializer.deserialize2(bundle.getString("category"));
         }
+
+        TextView catName = getView().findViewById(R.id.textView_childinfoedit_catName);
+        catName.setText(category.getName());
+
+        initInfo();
     }
 
     @Nullable
     @Override
     public View onCreateView(LayoutInflater inflater, @Nullable ViewGroup container, Bundle savedInstanceState) {
-        return inflater.inflate(R.layout.fragment_childinfo, container, false);
+        return inflater.inflate(R.layout.fragment_childinfo_edit, container, false);
     }
 
     @Override
@@ -37,11 +49,28 @@ public class ChildInfoEditFragment extends Fragment {
         setHasOptionsMenu(true);
     }
 
-    @Override
-    public void onActivityCreated(Bundle savedInstanceState) {
-        super.onActivityCreated(savedInstanceState);
-        if (savedInstanceState != null) {
+    private void initInfo() {
+        LinearLayout items = getView().findViewById(R.id.linearLayout_childinfoedit_info);
+        items.removeAllViews();
 
+        LayoutInflater inflater = getActivity().getLayoutInflater();
+        for(final Info i : category.getInfo()) {
+            ConstraintLayout item = (ConstraintLayout) inflater.inflate(R.layout.childinfoedit_item, null);
+            TextView name = item.findViewById(R.id.textView_childinfoedit_itemName);
+            name.setText(i.getName());
+            TextView value = item.findViewById(R.id.editText_childinfoedit_itemValue);
+            value.setText(i.getValue());
+
+            ImageButton remove = item.findViewById(R.id.imageButton_childinfoedit_remove);
+            remove.setOnClickListener(new View.OnClickListener() {
+                @Override
+                public void onClick(View view) {
+                    category.remove(i);
+                    initInfo();
+                }
+            });
+
+            items.addView(item);
         }
     }
 }
